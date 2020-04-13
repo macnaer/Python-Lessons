f56bb3631b96cd63f997a8d67c61fcdd42b0c8f8
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-04-13 20:06:55 +0300

    Added person liat example

--- 04. OOP Intro/start.py
+++ 04. OOP Intro/start.py
@@ -30,12 +30,12 @@ class Person:
 
 
 bill = Person("Billy", 56)
-print("Bill age before => ", bill.name)
-print("Bill age before => ", bill.age)
-bill.age = 58
+# print("Bill age before => ", bill.name)
+# print("Bill age before => ", bill.age)
+# bill.age = 58
 # bill.name = "Test" Error
-print("Bill age after => ", bill.name)
-print("Bill age after => ", bill.age)
+# print("Bill age after => ", bill.name)
+# print("Bill age after => ", bill.age)
 
 # bill.person_info()
 
@@ -44,5 +44,29 @@ print("Bill age after => ", bill.age)
 # bill.set_age(70)
 # print("After +> ", bill.get_age())
 
-# stive = Person("Stiven", 26)
+stive = Person("Stiven", 26)
 # stive.person_info()
+adam = Person("Adam", 34)
+eva = Person("Eva", 27)
+tina = Person("Tina", 29)
+sara = Person("Sara", 39)
+
+person_list = []
+person_list.append(bill)
+person_list.append(stive)
+person_list.append(adam)
+person_list.append(eva)
+person_list.append(tina)
+person_list.append(sara)
+
+
+for person in person_list:
+    # person.person_info()
+    print(person.name, " = ", person.age)
+    person.age += 1
+
+print("===========================")
+
+for person in person_list:
+    # person.person_info()
+    print(person.name, " = ", person.age)
